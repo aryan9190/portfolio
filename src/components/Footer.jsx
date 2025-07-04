d04f9e911118b6c018289d5dbf1c2b24bb068d2f
Aryan Yadav <115806023+aryan9190@users.noreply.github.com>
2025-07-04 16:50:06 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,8 @@ export default function Footer() {
         <footer className="text-center py-8 bg-black bg-opacity-80 mt-32 text-yellow-300">
             <p className="text-sm">© {new Date().getFullYear()} Aryan, el Nino</p>
             <div className="flex justify-center mt-2 gap-4 text-xl">
-                <a href="https://github.com/aryan9190" target="_blank" rel="noreferrer" aria-label="GitHub">🐙</a>
+                <a href="https://github.com/aryan9190" target="_blank" rel="noreferrer" aria-label="GitHub"><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="GitHub" className="w-6 h-6 mx-auto"/>
+</a>
             </div>
         </footer>
     );
