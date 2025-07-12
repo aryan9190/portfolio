d9f607090656b6f7ca38e3fbd480033646d1fa63
Aryan Yadav <115806023+aryan9190@users.noreply.github.com>
2025-07-12 08:36:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react', 'react-dom', 'react-router-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          'react-router-dom': 'ReactRouterDOM',
-        },
-      },
-    },
-  },
-});
+  base: './' 
+})
