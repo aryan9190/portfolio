88d2fb79df11bc257f4c8664418ce76da997a646
Aryan Yadav <115806023+aryan9190@users.noreply.github.com>
2025-07-15 19:12:52 +0530

    Update ParallaxBackground.jsx

--- src/components/ParallaxBackground.jsx
+++ src/components/ParallaxBackground.jsx
@@ -16,8 +16,8 @@ export default function ParallaxBackground() {
 
     return (
         <div className="pointer-events-none fixed inset-0 overflow-hidden -z-10">
-            <img ref={layer1} src="images/island.svg" alt="island" className="absolute bottom-0 left-1/4 w-1/2 opacity-20" />
-            <img ref={layer2} src="images/ship.svg" alt="ship" className="absolute bottom-10 right-1/3 w-1/3 opacity-30 animate slow-sway" />
+            <img ref={layer1} src="images/island.png" alt="island" className="absolute bottom-0 left-1/4 w-1/2 opacity-20" />
+            <img ref={layer2} src="images/ship.png" alt="ship" className="absolute bottom-10 right-1/3 w-1/3 opacity-30 animate slow-sway" />
         </div>
     );
 }
