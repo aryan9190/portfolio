989c2d1fa8b9e656f291d762fd10db87323e6c81
Aryan Yadav <115806023+aryan9190@users.noreply.github.com>
2025-07-15 18:52:38 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ export default function Contact () {
     return (
         <section id ="contact" className="py-32 px-6 text-center">
             <h2 className="text-4xl font-fraktur text-yellow-400 mb-4">📬 Message in a Bottle</h2>
-            <p className="text-lg text-gray-300">Ye can email me at <a href="mailto:example@example.com" className="underline"></a></p>
+            <p className="text-lg text-gray-300">Ye can email me <a href="mailto:example@example.com" className="underline">here</a></p>
         </section>
     );
 }
